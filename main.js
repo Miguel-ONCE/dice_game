c4bb614b525f7ec639f851acfe4612731bc698b7
Miguel_ONCE <linkin9ea@gmail.com>
2022-06-29 15:04:10 -0500

    tercer commit

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 
 if (randomNumber1 > randomNumber2){
-    document.querySelector("h1").textContent = "🚩 Player 1 wins!";
+    document.querySelector("h1").textContent = "🚩 Jugador 1 ganó!";
 }else if (randomNumber1 < randomNumber2){
-    document.querySelector("h1").textContent = "Player 2 wins! 🚩";
+    document.querySelector("h1").textContent = "Jugador 2 ganó! 🚩";
 }else{
-    document.querySelector("h1").textContent = "🚩 Draw! 🚩";
+    document.querySelector("h1").textContent = "🚩 Empate! 🚩";
 }
 
 document.querySelectorAll("div img")[0].setAttribute("src" ,`images/dice${randomNumber1}.png`);
@@ -20,36 +20,3 @@ document.querySelectorAll("div img")[1].setAttribute("src" ,`images/dice${random
 
 console.log(randomNumber1);
 console.log(randomNumber2);
-
-
-
-
-/* OPCIÓN DE ANGELA */
-// var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
-
-// var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
-
-// var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
-
-// var image1 = document.querySelectorAll("img")[0];
-
-// image1.setAttribute("src", randomImageSource);
-
-
-// var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-// var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-// document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
-
-// //If player 1 wins
-// if (randomNumber1 > randomNumber2) {
-//   document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
-// }
-// else if (randomNumber2 > randomNumber1) {
-//   document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-// }
-// else {
-//   document.querySelector("h1").innerHTML = "Draw!";
-// }
